cbb4c3d8ce36fbdb1dae3f12fe2d958575144259
AlexanderKurtsevich <alexander.kurtsevich@gmail.com>
2020-04-07 22:21:06 +0300

    feat: complete gem puzzle task

--- script.js
+++ script.js
@@ -3,17 +3,21 @@ class Puzzle {
         this.originalConfig = []
         this.config = [];
         this.movablePositions = [];
-        this.elements = [];
+        this.cells = [];
 
         this.countOfRows = 4;
-        this.countOfElements = 16;
+        this.countOfCells = 16;
         this.min = 0;
         this.sec = 0;
 
-        this.count = 0;  //Счетчик ходов
+        this.count = 0;
     }
 
     init() {
+        if (localStorage.results === undefined) {
+            localStorage.setItem('results', JSON.stringify([]))
+        }
+
         this.mainPuzzle = document.createElement('div');
         this.mainPuzzle.classList.add('puzzle');
         document.body.append(this.mainPuzzle);
@@ -23,17 +27,20 @@ class Puzzle {
 
         this.mainButtonsWrap = document.createElement('div');
         this.mainButtonsWrap.classList.add('puzzle__main-buttons-wrap');
+        this.mainButtonsWrap.append(this.timerButton());
+        this.mainButtonsWrap.append(this.newGameButton());
+        this.mainButtonsWrap.append(this.saveGameButton());
+        this.mainButtonsWrap.append(this.resultsButton());
         this.mainPuzzle.append(this.mainButtonsWrap);
-
-        this.createTimerButton();
-        this.createNewGameButton();
-        this.createSaveGameButton();
-        this.createResultsButton();
 
         this.container = document.createElement('div');
         this.container.classList.add('puzzle__container');
+        this.container.addEventListener('click', (event) => {
+            this.moveElement(event)
+        });
         this.mainPuzzle.append(this.container)
-        this.createElements();
+
+        this.createCells();
 
         this.zero = document.querySelector('.item-0');
 
@@ -41,51 +48,12 @@ class Puzzle {
         this.blockWindow.classList.add('puzzle__block-window', 'active');
         this.container.append(this.blockWindow);
 
-        this.mainPuzzle.append(this.createSizeButtons());
-        this.container.addEventListener('click', (event) => {
-            this.moveElement(event)
-        });
-        this.sizeButtons.addEventListener('click', (event) => { 
-            if (event.target.classList.contains('button')){    //Ивент листенер для кнопок размеров поля
-            this.countOfRows = event.target.innerHTML[0];
-            this.countOfElements = this.countOfRows ** 2;
-            this.refreshGame()
-            }
-        })
-        this.timerButton.addEventListener('click', (event) => {     //Ивент листенер для кнопки таймера (старт стоп)
-            if (this.timerButton.innerHTML === 'СТАРТ') {
-                this.startTimer();
-            }
-            else {
-                this.stopTimer()
-            }
-            this.timerButton.innerHTML = this.timerButton.innerHTML === 'СТАРТ' ? 'СТОП' : 'СТАРТ';
-            this.blockWindow.classList.toggle('active')
-        })
-
-        this.newGameButton.addEventListener('click', (event) => {
-            this.refreshGame();
-        })
-
-        this.saveGameButton.addEventListener('click', (event) => {
-            let gameInfo = {
-                'countOfRows': this.countOfRows,
-                'countOfElements': this.countOfElements,
-                'config': this.config,
-                'min': this.min,
-                'sec': this.sec,
-                'count': this.count,
-            };
-            localStorage.setItem('save', true)
-            localStorage.setItem('gameInfo', JSON.stringify(gameInfo))
-            
-        })
-
+        this.mainPuzzle.append(this.sizeButtons());
     }
 
-    
-    createElements() {
-        for (let i = 0; i <= this.countOfElements - 1; i += 1) {
+
+    createCells() {
+        for (let i = 0; i <= this.countOfCells - 1; i += 1) {
             this.config.push(i.toString())
             this.originalConfig.push(i.toString())
         }
@@ -100,27 +68,99 @@ class Puzzle {
         if (localStorage.save === "true") {
             this.config = JSON.parse(localStorage.gameInfo).config;
             this.countOfRows = JSON.parse(localStorage.gameInfo).countOfRows;
-            this.countOfElements = JSON.parse(localStorage.gameInfo).countOfElements;
+            this.countOfCells = JSON.parse(localStorage.gameInfo).countOfCells;
             localStorage.save = "false"
         }
 
         this.config.forEach((number) => {
-            let box = document.createElement('div');
-            box.classList.add('puzzle__box', `item-${number}`);
-            box.innerHTML = number;
-            box.style.left = 0;
-            box.style.top = 0;
-            this.container.append(box);
-            this.elements.push(box);
+            let cell = document.createElement('div');
+            cell.classList.add('puzzle__cell', `item-${number}`);
+            cell.innerHTML = number;
+            cell.style.left = 0;
+            cell.style.top = 0;
+            this.container.append(cell);
+            this.cells.push(cell);
         })
 
         this.container.style.gridTemplateColumns = "1fr ".repeat(this.countOfRows);
     }
 
-    removeElements() {
+    sizeButtons() {
+        this.sizeButtons = document.createElement('div');
+        this.sizeButtons.classList.add('puzzle__size-buttons')
+        for (let i = 3; i <= 8; i++) {
+            let button = document.createElement('button');
+            button.classList.add('puzzle__size-button', 'button')
+            button.innerHTML = (`${i}x${i}`);
+            this.sizeButtons.append(button)
+        }
+
+        this.sizeButtons.addEventListener('click', (event) => {
+            if (event.target.classList.contains('button')) {
+                this.countOfRows = event.target.innerHTML[0];
+                this.countOfCells = this.countOfRows ** 2;
+                this.refreshGame()
+            }
+        })
+
+        return this.sizeButtons;
+    }
+
+    timerButton() {
+        this.timerButton = document.createElement('button');
+        this.timerButton.classList.add('puzzle__timer-button', 'button');
+        this.timerButton.innerHTML = 'СТАРТ';
+        this.timerButton.addEventListener('click', (event) => {
+            (this.timerButton.innerHTML === 'СТАРТ') ? this.startTimer() : this.stopTimer();
+            this.timerButton.innerHTML = this.timerButton.innerHTML === 'СТАРТ' ? 'СТОП' : 'СТАРТ';
+            this.blockWindow.classList.toggle('active')
+        })
+        return this.timerButton;
+    }
+
+    newGameButton() {
+        this.newGameButton = document.createElement('button');
+        this.newGameButton.classList.add('puzzle__new-game-button', 'button');
+        this.newGameButton.innerHTML = "Перемешать";
+        this.newGameButton.addEventListener('click', (event) => {
+            this.refreshGame();
+        })
+        return this.newGameButton;
+    }
+
+    saveGameButton() {
+        this.saveGameButton = document.createElement('button');
+        this.saveGameButton.classList.add('puzzle__save-game-button', 'button');
+        this.saveGameButton.innerHTML = "Сохранить";
+        this.saveGameButton.addEventListener('click', (event) => {
+            let gameInfo = {
+                'countOfRows': this.countOfRows,
+                'countOfCells': this.countOfCells,
+                'config': this.config,
+                'min': this.min,
+                'sec': this.sec,
+                'count': this.count,
+            };
+            localStorage.setItem('save', true)
+            localStorage.setItem('gameInfo', JSON.stringify(gameInfo))
+        })
+        return this.saveGameButton;
+    }
+
+    resultsButton() {
+        this.resultsButton = document.createElement('button');
+        this.resultsButton.classList.add('puzzle__results-button', 'button');
+        this.resultsButton.innerHTML = "Результаты";
+        this.resultsButton.addEventListener('click', (event) => {
+            this.showResults()
+        })
+        return this.resultsButton;
+    }
+
+    removeCells() {
         this.config = [];
         this.originalConfig = []
-        this.elements.forEach(elem => {
+        this.cells.forEach(elem => {
             elem.remove()
         });
     }
@@ -143,20 +183,9 @@ class Puzzle {
         this.countElement.innerHTML = `Количество ходов: ${this.count}`;
     }
 
-    createSizeButtons() {
-        this.sizeButtons = document.createElement('div');
-        this.sizeButtons.classList.add('puzzle__size-buttons')
-        for (let i = 3; i <= 8; i++) {
-            let button = document.createElement('button');
-            button.classList.add('puzzle__size-button', 'button')
-            button.innerHTML = (`${i}x${i}`);
-            this.sizeButtons.append(button)
-        }
-        return this.sizeButtons;
-    }
+
 
     createTimer() {
-
         this.timer = document.createElement('p');
         if (localStorage.save === "true") {
             this.min = JSON.parse(localStorage.gameInfo).min;
@@ -169,15 +198,9 @@ class Puzzle {
         this.mainPuzzle.append(this.timer);
     }
 
-    createTimerButton() {
-        this.timerButton = document.createElement('button');
-        this.timerButton.classList.add('puzzle__timer-button', 'button');
-        this.timerButton.innerHTML = 'СТАРТ'
-        this.mainButtonsWrap.append(this.timerButton);
-    }
-
     startTimer() {
         this.runningTimer = setInterval(() => {
+
             if (this.sec < 9) {
                 this.timer.innerHTML = `Время: ${this.min}:0${this.sec += 1}`;
             }
@@ -185,9 +208,10 @@ class Puzzle {
                 this.timer.innerHTML = `Время: ${this.min}:${this.sec += 1}`
             }
 
-            if (this.sec === 59) {
+            if (this.sec === 60) {
                 this.sec = 0;
-                this.min += 1
+                this.min += 1;
+                this.timer.innerHTML = `Время: ${this.min}:0${this.sec}`;
             }
         }, 1000);
     }
@@ -203,53 +227,32 @@ class Puzzle {
         this.timerButton.innerHTML = 'СТАРТ';
     }
 
-    createNewGameButton() {
-        this.newGameButton = document.createElement('button');
-        this.newGameButton.classList.add('puzzle__new-game-button', 'button');
-        this.newGameButton.innerHTML = "Перемешать";
-        this.mainButtonsWrap.append(this.newGameButton);
-    }
-
-    createSaveGameButton() {
-        this.saveGameButton = document.createElement('button');
-        this.saveGameButton.classList.add('puzzle__save-game-button', 'button');
-        this.saveGameButton.innerHTML = "Сохранить";
-        this.mainButtonsWrap.append(this.saveGameButton);
-    }
-
-    createResultsButton() {
-        this.ResultsButton = document.createElement('button');
-        this.ResultsButton.classList.add('puzzle__results-button', 'button');
-        this.ResultsButton.innerHTML = "Результаты";
-        this.mainButtonsWrap.append(this.ResultsButton);
-    }
-
     changePositionsInConfig() {
         [this.config[this.config.indexOf(event.target.innerHTML)], this.config[this.positionOfZero]] = [this.config[this.positionOfZero], this.config[this.config.indexOf(event.target.innerHTML)]]
     }
 
-    refreshGame(){
+    refreshGame() {
         this.stopTimer();
         this.refreshTimer();
         this.refreshCount()
-        this.removeElements();
-        this.createElements();
+        this.removeCells();
+        this.createCells();
         this.blockWindow.classList.add('active');
     }
 
     moveElement() {
         this.positionOfZero = this.config.indexOf('0');
 
-        let elementWidth = event.target.getBoundingClientRect().width + 3
+        let cellWidth = event.target.getBoundingClientRect().width + 3
 
-        if (event.target.innerHTML >= 0 && event.target.innerHTML <= this.countOfElements - 1) {
+        if (event.target.innerHTML >= 0 && event.target.innerHTML <= this.countOfCells - 1) {
             switch (event.target.innerHTML) {
                 case this.config[this.positionOfZero - 1]: {
                     if (this.positionOfZero % this.countOfRows == 0) {
                         break;
                     }
-                    event.target.style.left = parseFloat(event.target.style.left) + elementWidth + 'px';
-                    this.zero.style.left = parseFloat(this.zero.style.left) - elementWidth + 'px';
+                    event.target.style.left = parseFloat(event.target.style.left) + cellWidth + 'px';
+                    this.zero.style.left = parseFloat(this.zero.style.left) - cellWidth + 'px';
 
                     this.changePositionsInConfig();
                     this.incremetCount();
@@ -259,30 +262,30 @@ class Puzzle {
                     if ((this.positionOfZero + 1) % this.countOfRows == 0) {
                         break;
                     }
-                    event.target.style.left = parseFloat(event.target.style.left) - elementWidth + 'px';
-                    this.zero.style.left = parseFloat(this.zero.style.left) + elementWidth + 'px';
+                    event.target.style.left = parseFloat(event.target.style.left) - cellWidth + 'px';
+                    this.zero.style.left = parseFloat(this.zero.style.left) + cellWidth + 'px';
                     this.changePositionsInConfig();
                     this.incremetCount();
                     break;
                 };
                 case this.config[this.positionOfZero + -this.countOfRows]: {
-                    event.target.style.top = parseFloat(event.target.style.top) + elementWidth + 'px';
-                    this.zero.style.top = parseFloat(this.zero.style.top) - elementWidth + 'px';
+                    event.target.style.top = parseFloat(event.target.style.top) + cellWidth + 'px';
+                    this.zero.style.top = parseFloat(this.zero.style.top) - cellWidth + 'px';
                     this.changePositionsInConfig();
                     this.incremetCount();
                     break;
                 };
                 case this.config[this.positionOfZero + +this.countOfRows]: {
 
-                    event.target.style.top = parseFloat(event.target.style.top) - elementWidth + 'px';
-                    this.zero.style.top = parseFloat(this.zero.style.top) + elementWidth + 'px';
+                    event.target.style.top = parseFloat(event.target.style.top) - cellWidth + 'px';
+                    this.zero.style.top = parseFloat(this.zero.style.top) + cellWidth + 'px';
                     this.changePositionsInConfig();
                     this.incremetCount();
                     break;
                 };
             }
         }
-        this.check();
+        this.checkWin();
     }
 
     showWinWindow() {
@@ -297,36 +300,76 @@ class Puzzle {
         this.inputName = document.createElement('input');
         this.inputName.classList.add('win-window__inputName');
         this.inputName.setAttribute('placeholder', 'Введите ваше имя');
-        this.inputName.setAttribute('maxlength', '16')
+        this.inputName.setAttribute('maxlength', '12');
         this.winWindow.append(this.inputName);
 
         this.winApplyButton = document.createElement('button');
         this.winApplyButton.classList.add('win-window__apply-button', 'button');
         this.winApplyButton.innerHTML = 'Принять';
+        this.winApplyButton.addEventListener('click', (event) => {
+            let resultsArray = JSON.parse(localStorage.results);
+            if (this.inputName.value === "") {
+                this.inputName.value = 'Аноним'
+            }
+            resultsArray.push({ name: this.inputName.value, turns: `Ходов: ${this.count}`, time: `Время: ${this.min}м. ${this.sec}с.`, sec: ((this.min * 60) + this.sec) });
+            resultsArray.sort((elemA, elemB) => {
+                return elemA.sec - elemB.sec;
+            })
+            if (resultsArray.length > 9) {
+                resultsArray = resultsArray.slice(0, 10);
+            }
+            localStorage.results = JSON.stringify(resultsArray)
+
+            this.refreshGame();
+            this.winWindow.remove();
+            block.remove()
+
+        })
         this.winWindow.append(this.winApplyButton);
 
         let block = document.createElement('div');
         block.classList.add('block-all');
         document.body.append(block);
 
-
-        this.winApplyButton.addEventListener('click', (event) => {
-            this.refreshGame();
-            this.winWindow.remove();
-            block.remove()
-
-        })
         document.body.append(this.winWindow)
     }
 
-    showResults(){
-        
+    showResults() {
+        this.resultsWindow = document.createElement('div');
+        this.resultsWindow.classList.add('results-window');
+        this.resultsWindow.innerHTML = `<p>Результаты</p>`;
+
+        let resultsArray = JSON.parse(localStorage.results);
+        if (resultsArray.length !== 0) {
+            resultsArray.forEach((obj, index) => {
+                let playerInfo = document.createElement('p');
+                playerInfo.classList.add('results-window__player-info')
+                playerInfo.innerHTML = `${index + 1}. <b>${obj.name}</b> ${obj.time} ${obj.turns}`
+                this.resultsWindow.append(playerInfo)
+            })
+        }
+        else {
+            let noResults = document.createElement('p');
+            noResults.innerHTML = 'Результатов нет.';
+            noResults.style.fontSize = "14px"
+            this.resultsWindow.append(noResults)
+        }
+
+        this.resultsCloseButton = document.createElement('button');
+        this.resultsCloseButton.classList.add('results-window__close-button', 'button');
+        this.resultsCloseButton.innerHTML = 'Закрыть';
+        this.resultsCloseButton.addEventListener('click', (event) => {
+            this.resultsWindow.remove()
+        })
+        this.resultsWindow.append(this.resultsCloseButton);
+
+        document.body.append(this.resultsWindow);
     }
 
-    check() {
+    checkWin() {
         let checkCount = 0;
 
-        for (let i = 0; i < this.countOfElements; i++) {
+        for (let i = 0; i < this.countOfCells; i++) {
             (this.originalConfig[i] === this.config[i]) ? checkCount += 1 : checkCount = 0;
         }
 
